df3cfee438b7fb75a6617d3cb923d469a79fdadc
kadhir812 <kadhir555666@gmail.com>
2025-06-28 14:31:37 +0530

    fixing frontend lints and set tests3

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,41 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
 
 export default [
-  { ignores: ['dist'] },
+  ...js(),
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-  js.config({
+    files: ['**/*.jsx', '**/*.js'],
+    plugins: { react },
     languageOptions: {
       globals: {
         // Jest globals
@@ -51,12 +21,8 @@ export default [
         document: true,
       },
     },
-  }),
-  {
-    files: ['**/*.jsx', '**/*.js'],
-    plugins: { react },
     rules: {
       // your rules here
     },
   },
-]
+];
